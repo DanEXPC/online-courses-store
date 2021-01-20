3dbda35676db9e4b3b84507f4647efc83a57968f
Danylo <danylosmahliuk@gmail.com>
2021-01-20 15:55:34 +0200

    Data rendering

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const path = require('path')
 const exphbs = require('express-handlebars')
 
 const app = express()
@@ -16,11 +15,24 @@ app.set('views', 'views')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: 'Home page',
+        isHome: true
+    })
 })
 
-app.get('/about', (req, res) => {
-    res.render('about')
+app.get('/add', (req, res) => {
+    res.render('add', {
+        title: 'Add course',
+        isAdd: true
+    })
+})
+
+app.get('/courses', (req, res) => {
+    res.render('courses', {
+        title: 'Courses',
+        isCourses: true
+    })
 })
 
 
