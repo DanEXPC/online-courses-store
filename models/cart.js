d5a1ff1839daf331dfdae1f00485378ae28300e6
Danylo <danylosmahliuk@gmail.com>
2021-01-21 12:30:46 +0200

    Creating Cart model

--- models/cart.js
+++ models/cart.js
@@ -1,9 +1,43 @@
+const path = require('path')
+const fs = require('fs')
 class Cart {
-    add() {
+    static async add(course) {
+        const cart = await Cart.fetch()
 
+        const idx = cart.courses.findIndex(c => c.id === course.id)
+        const candidate = cart.courses[idx]
+
+        if(candidate) {
+            candidate.count++
+            cart.courses[idx] = candidate
+        } else {
+            course.count = 1
+            cart.courses.push(course)
+        }
+
+        cart.price += +course.price
+
+        return new Promise((resolve, reject) => {
+            fs.writeFile(path.join(__dirname, '..', 'data', 'cart.json'), JSON.stringify(cart), err => {
+                if (err) {
+                    refect(err)
+                } else {
+                    resolve()
+                }
+            })
+        })
     }
-    fetch() {
 
+    static async fetch() {
+        return new Promise((resolve, reject) => {
+            fs.readFile(path.join(__dirname, '..', 'data', 'cart.json'), (err, content) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(JSON.parse(content))
+                }
+            })
+        })
     }
 }
 
