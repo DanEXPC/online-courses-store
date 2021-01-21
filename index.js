cf30285f42fa326b19b0ed530140ff1cbd32cd9f
Danylo <danylosmahliuk@gmail.com>
2021-01-21 12:14:06 +0200

    Shopping cart preparation [fixed]

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home') 
 const addRoutes = require('./routes/add') 
-const cardRoutes = require('./routes/card') 
+const cartRoutes = require('./routes/cart') 
 const coursesRoutes = require('./routes/courses') 
 
 const app = express()
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended: true}))
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
-app.use('/card', cardRoutes)
+app.use('/cart', cartRoutes)
 
 const PORT = process.env.PORT || 3000
 
--- routes/cart.js
+++ routes/cart.js
@@ -1,19 +1,19 @@
 const {Router} = require('express')
-const Card = require('../models/card')
+const Cart = require('../models/cart')
 const Course = require('../models/course')
 const router = Router()
 
 router.post('/add', async (req, res) => {
     const course = await Course.getById(req.body.id)
-    await Card.add(course)
-    res.redirect('/card')
+    await Cart.add(course)
+    res.redirect('/cart')
 })
 
 router.get('/', async (req, res) => {
-    const card = await Card.fetch()
-    res.render('card', {
-        title: 'Card',
-        card
+    const cart = await Cart.fetch()
+    res.render('cart', {
+        title: 'Cart',
+        cart
     })
 })
 
