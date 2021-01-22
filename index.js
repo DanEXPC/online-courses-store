ef5290824291dd7612e18b2d543326407451c834
Danylo <danylosmahliuk@gmail.com>
2021-01-22 14:07:57 +0200

    User addition

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home') 
 const addRoutes = require('./routes/add') 
 const cartRoutes = require('./routes/cart') 
-const coursesRoutes = require('./routes/courses') 
+const coursesRoutes = require('./routes/courses')
+const User = require('./models/User') 
 
 const app = express()
 
@@ -22,8 +23,19 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(async (req, res, next) => {
+    try {
+        const user = await User.findById('600abb2a330a820b2883c8b0')
+        req.user = user
+        next()
+    } catch (err) {
+        console.log(err)
+    }  
+})
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
+
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
@@ -40,6 +52,15 @@ async function start() {
             useUnifiedTopology: true,
             useFindAndModify: false
         })
+        const candidate = await User.findOne()
+        if (!candidate) {
+            const user = new User({
+                email: 'danylosmahliuk@gmail.com',
+                name: 'Danylo',
+                cart: {items: []}
+            })
+            await user.save()
+        }
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`)
         })
