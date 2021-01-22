ffffd2ac41b330eeecc766b36244ac9af7f0b94e
Danylo <danylosmahliuk@gmail.com>
2021-01-22 14:49:19 +0200

    Displaying a cart

--- routes/cart.js
+++ routes/cart.js
@@ -2,6 +2,19 @@ const {Router} = require('express')
 const Course = require('../models/course')
 const router = Router()
 
+function mapCartItems(cart) {
+    return cart.items.map(c => ({
+        ...c.courseId._doc, 
+        count: c.count
+    }))
+}
+
+function computePrice(courses) {
+    return courses.reduce((total, course) => {
+        return total += course.price * course.count
+    }, 0)
+}
+
 router.post('/add', async (req, res) => {
     const course = await Course.findById(req.body.id)
     await req.user.addToCart(course)
@@ -14,15 +27,18 @@ router.delete('/remove/:id', async (req, res) => {
 })
 
 router.get('/', async (req, res) => {
-    // const cart = await Cart.fetch()
-    // res.render('cart', {
-    //     title: 'Cart',
-    //     isCart: true,
-    //     courses: cart.courses,
-    //     price: cart.price
-    // })
+    const user = await req.user
+        .populate('cart.items.courseId')
+        .execPopulate()
 
-    res.json({test: true})
+    const courses = mapCartItems(user.cart)
+
+    res.render('cart', {
+        title: 'Cart',
+        isCart: true,
+        courses: courses,
+        price: computePrice(courses)
+    })
 })
 
 module.exports = router
