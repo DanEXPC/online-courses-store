12733af1470bad73e0a75f6fbf37720276b71b4f
Danylo <danylosmahliuk@gmail.com>
2021-01-25 10:10:16 +0200

    Fixed bugs with cart using middleware

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const coursesRoutes = require('./routes/courses')
 const ordersRoutes = require('./routes/orders')
 const authRoutes = require('./routes/auth')
 const varMiddleware = require('./middleware/variables')
+const userMiddleware = require('./middleware/user')
 
 const MONGODB_URI = 'mongodb+srv://danylo:T4mMQ0F5aDSJziYq@cluster0.r3a4j.mongodb.net/shop?retryWrites=true&w=majority'
 
@@ -42,6 +43,7 @@ app.use(session({
     store
 }))
 app.use(varMiddleware)
+app.use(userMiddleware)
 
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
