0265e72e31702bc28b90cf79dc76fc6127e7450a
Danylo <danylosmahliuk@gmail.com>
2021-01-26 10:09:47 +0200

    Password change

--- routes/auth.js
+++ routes/auth.js
@@ -144,4 +144,28 @@ router.post('/reset', (req, res) => {
     }
 })
 
+router.post('/password', async (req, res) => {
+    try {
+        const user = await User.findOne({ 
+            _id: req.body.userId,
+            resetToken: req.body.token,
+            resetTokenExp: {$gt: Date.now()}
+        })
+
+        if (user) {
+            user.password = await bcrypt.hash(req.body.password, 10)
+            user.resetToken = undefined
+            user.resetTokenExp = undefined
+            await user.save()
+            res.redirect('/auth/login')
+        } else {
+            await req.flash('loginError', 'Token’s lifetime is over')
+            res.redirect('/auth/login')
+        } 
+         
+    } catch (err) {
+        console.log(err)
+    }
+})
+
 module.exports = router
