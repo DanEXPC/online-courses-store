fc9afabeea3b9ce4da26723ce4db739d5280e240
Danylo <danylosmahliuk@gmail.com>
2021-01-27 10:44:13 +0200

    Improve data before sending to database

--- utils/validators.js
+++ utils/validators.js
@@ -2,22 +2,30 @@ const {body} = require('express-validator')
 const User = require('../models/user')
 
 exports.registerValidators = [
-    body('email').isEmail().withMessage('Enter correct email address').custom( async (value, {req}) => {
-        try {
-            const user = await User.findOne({ email: value })
-            if (user) {
-                return Promise.reject('User with such email already registred')
+    body('email')
+        .isEmail().withMessage('Enter correct email address')
+        .custom( async (value, {req}) => {
+            try {
+                const user = await User.findOne({ email: value })
+                if (user) {
+                    return Promise.reject('User with such email already registred')
+                }
+            } catch (err) {
+                    console.log(err)
             }
-        } catch (err) {
-                console.log(err)
-        }
-    }),
-    body('password', 'Password must be at least 6 characters long').isLength({min: 6, max: 56}).isAlphanumeric(),
+        })
+        .normalizeEmail(),
+    body('password', 'Password must be at least 6 characters long')
+        .isLength({min: 6, max: 56}).isAlphanumeric()
+        .trim(),
     body('confirm').custom((value, {req}) => {
         if (value !== req.body.password) {
             throw new Error('Passwords must be the same')
         }
         return true
-    }),
-    body('name').isLength({min: 3}).withMessage('Name must be at least 3 characters long')
+    })
+    .trim(),
+    body('name').isLength({min: 3})
+        .withMessage('Name must be at least 3 characters long')
+        .trim()
 ]
