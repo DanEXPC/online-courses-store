7505621ad333dd9e2fca5e13f4d48a80c9032a66
Danylo <danylosmahliuk@gmail.com>
2021-01-27 16:41:50 +0200

    Loading profile image

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const profileRoutes = require('./routes/profile')
 const varMiddleware = require('./middleware/variables')
 const userMiddleware = require('./middleware/user')
 const errorHandler = require('./middleware/error')
+const fileMiddleware = require('./middleware/file')
 const keys = require('./keys')
 
 const app = express()
@@ -39,6 +40,7 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use(express.urlencoded({extended: true}))
 app.use(session({
     secret: keys.SESSION_SECRET,
@@ -46,6 +48,7 @@ app.use(session({
     saveUninitialized: false,
     store
 }))
+app.use(fileMiddleware.single('avatar'))
 app.use(csrf())
 app.use(flash({ sessionKeyName: keys.SESSION_SECRET }))
 app.use(varMiddleware)
